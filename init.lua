bf0a1debe2ee409919691123bcd9d10e9ec33c79
anas jaidi <anas.jaidi@icloud.com>
2024-06-03 08:18:40 +0100

    sync: _

--- init.lua
+++ init.lua
@@ -217,7 +217,6 @@ if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -241,7 +240,22 @@ require("lazy").setup({
 	--
 	--  This is equivalent to:
 	--    require('Comment').setup({})
-
+	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"Diogo-ss/42-header.nvim",
+		cmd = { "Stdheader" },
+		keys = { "<F1>" },
+		opts = {
+			default_map = true, -- Default mapping <F1> in normal mode.
+			auto_update = true, -- Update header when saving.
+			user = "ajaidi", -- Your user.
+			mail = "ajaidi@student.42.fr", -- Your mail.
+			-- add other options.
+		},
+		config = function(_, opts)
+			require("42header").setup(opts)
+		end,
+	},
 	{
 		"nyoom-engineering/oxocarbon.nvim",
 		config = function()
@@ -256,6 +270,65 @@ require("lazy").setup({
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		opts = {},
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			position = "bottom", -- position of the list can be: bottom, top, left, right
+			height = 10, -- height of the trouble list when position is top or bottom
+			width = 50, -- width of the list when position is left or right
+			icons = true, -- use devicons for filenames
+			mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+			severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+			fold_open = "", -- icon used for open folds
+			fold_closed = "", -- icon used for closed folds
+			group = true, -- group results by file
+			padding = true, -- add an extra new line on top of the list
+			cycle_results = true, -- cycle item list when reaching beginning or end of list
+			action_keys = { -- key mappings for actions in the trouble list
+				-- map to {} to remove a mapping, for example:
+				-- close = {},
+				close = "q", -- close the list
+				cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+				refresh = "r", -- manually refresh
+				jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
+				open_split = { "<c-x>" }, -- open buffer in new split
+				open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+				open_tab = { "<c-t>" }, -- open buffer in new tab
+				jump_close = { "o" }, -- jump to the diagnostic and close the list
+				toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+				switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+				toggle_preview = "P", -- toggle auto_preview
+				hover = "K", -- opens a small popup with the full multiline message
+				preview = "p", -- preview the diagnostic location
+				open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
+				close_folds = { "zM", "zm" }, -- close all folds
+				open_folds = { "zR", "zr" }, -- open all folds
+				toggle_fold = { "zA", "za" }, -- toggle fold of current file
+				previous = "k", -- previous item
+				next = "j", -- next item
+				help = "?", -- help menu
+			},
+			multiline = true, -- render multi-line messages
+			indent_lines = true, -- add an indent guide below the fold icons
+			win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
+			auto_open = false, -- automatically open the list when you have diagnostics
+			auto_close = false, -- automatically close the list when you have no diagnostics
+			auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+			auto_fold = false, -- automatically fold a file trouble list at creation
+			auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
+			include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions" }, -- for the given modes, include the declaration of the current symbol in the results
+			signs = {
+				-- icons / text used for a diagnostic
+				error = "",
+				warning = "",
+				hint = "",
+				information = "",
+				other = "",
+			},
+			use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+		},
 	},
 
 	-- Here is a more advanced example where we pass configuration
@@ -438,7 +511,30 @@ require("lazy").setup({
 			end, { silent = true })
 			return require("nvim-tree").setup({
 				open_on_setup = true,
+				git = {
+					ignore = false,
+				},
 			})
+		end,
+	},
+	{
+		"simrat39/rust-tools.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			local opts = {
+				server = {
+					settings = {
+						["rust-analyzer"] = {
+							checkOnSave = {
+								command = "clippy",
+							},
+						},
+					},
+				},
+			}
+			require("rust-tools").setup(opts)
 		end,
 	},
 	{ -- LSP Configuration & Plugins
@@ -448,7 +544,6 @@ require("lazy").setup({
 			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-
 			-- Useful status updates for LSP.
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
@@ -611,6 +706,14 @@ require("lazy").setup({
 				jsonls = {},
 				yamlls = {},
 				dockerls = {},
+				solang = {},
+				solidity_ls = {},
+				sqlls = {
+					filetypes = { "sql" },
+					root_dir = function(_)
+						return vim.loop.cwd()
+					end,
+				},
 				tsserver = {
 					filetypes = {
 						"javascript",
@@ -673,17 +776,20 @@ require("lazy").setup({
 			--    :Mason
 			--
 			--  You can press `g?` for help in this menu.
-			require("mason").setup()
 
+			require("mason").setup()
 			-- You can add other tools here that you want Mason to install
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
 				"tsserver",
+				"solang",
+				"solidity_ls",
 				"rust_analyzer",
 				"tailwindcss",
 				"cssls",
+				"sqlls",
 				"html",
 				"yamlls",
 				"dockerls",
@@ -1001,3 +1107,22 @@ vim.keymap.set("n", "<leader>tp", "<cmd>lua _PYTHON_TOGGLE()<cr>", {})
 vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", {})
 vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", {})
 vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", {})
+-- Lua
+vim.keymap.set("n", "<leader>xx", function()
+	require("trouble").toggle()
+end)
+vim.keymap.set("n", "<leader>xw", function()
+	require("trouble").toggle("workspace_diagnostics")
+end)
+vim.keymap.set("n", "<leader>xd", function()
+	require("trouble").toggle("document_diagnostics")
+end)
+vim.keymap.set("n", "<leader>xq", function()
+	require("trouble").toggle("quickfix")
+end)
+vim.keymap.set("n", "<leader>xl", function()
+	require("trouble").toggle("loclist")
+end)
+vim.keymap.set("n", "gR", function()
+	require("trouble").toggle("lsp_references")
+end)
